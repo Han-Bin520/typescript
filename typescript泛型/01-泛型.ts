0213d63a4ad76164cd2ee95f04e4357a9b880f2b
Han-Bin520 <1263256614@qq.com>
2020-10-05 17:38:26 +0800

    ts泛型约束

--- typescript泛型/01-泛型.ts
+++
@@ -1,8 +0,0 @@
-let getArray = <T>(value:T, item:number = 5):T[] =>{
-    return new Array(item).fill(value);
-};
-
-let arr = getArray<string>("abc");
-let resH = arr.map(item=>item.length);
-console.log(resH);
-
